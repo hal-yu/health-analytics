23ff5af62676814f4693833b82d5ca5f7fcf648f
yu-hal <haley.yu@stonybrook.edu>
2023-08-17 17:37:56 +0000

    added a dictionary

--- health_analysis.py
+++ health_analysis.py
@@ -9,3 +9,16 @@ String = "String"
 
 # List
 List = [1, 2, 3, 4,5]
+
+# Dictionary
+Yu_Dictionary = {
+    "name": "Haley",
+    "age": 23,
+    "contacts": "haley.yu@stonybrook.edu",
+    "address": {
+        "Country": "United States",
+        "State": "New York",
+        "County": "Queeens",
+        "Zip Code": "10000"
+    }
+}
