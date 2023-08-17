9430a147764b031240703533f26f34e56c7d3ea6
yu-hal <haley.yu@stonybrook.edu>
2023-08-17 19:29:24 +0000

    added a function

--- health_analysis.py
+++ health_analysis.py
@@ -15,7 +15,7 @@ Yu_Dictionary = {
     "FirstName": "Haley",
     "LastName": "Yu",
     "age": 23,
-    "contacts": ["haley.yu@stonybrook.edu", "718-718-0718"]
+    "contacts": ["haley.yu@stonybrook.edu", "718-718-0718"],
     "address": {
         "Country": "United States",
         "State": "New York",
@@ -23,3 +23,28 @@ Yu_Dictionary = {
         "Zip Code": "10000"
     }
 }
+
+# Function
+def gradePointAverage(HHA506, HHA503):
+    # Check Research GPA First
+    if HHA506 >= 93:
+        HHA506_output = 'good'
+    else:
+        HHA506_output = 'bad'
+    # Check HIPAA GPA Second
+    if HHA503 >= 93:
+        HHA503_output = 'good'
+    else:
+        HHA503_output = 'bad'
+    output = [HHA506_output, HHA503_output]
+    return output
+    
+HHA506Input = 94
+HHA503Input = 92
+
+function_output = gradePointAverage(HHA506Input, HHA503Input)
+
+print('HHA 506 GPA: ', HHA506Input)
+print('HHA506 Interpretation: ', function_output[0])
+print('HHA 503 GPA: ', HHA503Input)
+print('HHA503 Interpretation: ', function_output[1])
